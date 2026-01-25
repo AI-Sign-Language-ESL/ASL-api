4ee9cd4b47a6026fe786a61530ad08042c41f6e4
Mo-UNO <mohamedamir4491@gmail.com>
2026-01-25 23:41:37 +0200

    fixes

--- src/tafahom_api/asgi.py
+++ src/tafahom_api/asgi.py
@@ -1,19 +1,22 @@
 import os
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter  # noqa: E402
+from channels.security.websocket import AllowedHostsOriginValidator  # noqa: E402
+from tafahom_api.apps.v1.translation.routing import websocket_urlpatterns  # noqa: E402
+from tafahom_api.apps.v1.authentication.middleware import (
+    JWTAuthMiddlewareStack,
+)  # noqa: E402
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tafahom_api.settings")
 
-# Initialize Django ASGI application
 django_asgi_app = get_asgi_application()
 
-# Import AFTER Django setup
-from channels.routing import ProtocolTypeRouter, URLRouter  # noqa: E402
-from tafahom_api.apps.v1.translation.routing import websocket_urlpatterns  # noqa: E402
-from tafahom_api.middleware.jwt_auth import JWTAuthMiddlewareStack  # noqa: E402
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": JWTAuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+        "websocket": AllowedHostsOriginValidator(
+            JWTAuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
     }
 )
